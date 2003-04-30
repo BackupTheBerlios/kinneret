e75acfcb5e0744676e1a0fe96f72bcaa4a181d3e
z9u2k <z9u2k>
2003-04-30 11:16:38 +0000

    0.1 - Final Testing Phase

--- confwiz/confwiz/pleasewait.ui.h
+++ confwiz/confwiz/pleasewait.ui.h
@@ -6,10 +6,11 @@
 ** init() function in place of a constructor, and a destroy() function in
 ** place of a destructor.
 *****************************************************************************/
-
+#include <fstream>
 
 void pleasewait::onTimer()
 {
+	// update leds
 	nLed++;
 	if (nLed == 5) nLed = 0;	// warp
 
@@ -22,4 +23,55 @@ void pleasewait::onTimer()
 		if (i == nLed) leds[i]->on();
 		else leds[i]->off();
 	}
+
+	// update text
+	QString qMsg;
+	std::ifstream f("/tmp/.progress");
+	char szLine[0x300];
+	f.getline(szLine, 0x300);
+	f.close();
+
+	QString qLine = QString(szLine);
+	if (qLine == QString("DIR_CREATE"))
+		qMsg = QString(tr2i18n("Creating directory... [1/13]"));
+	
+	else if (qLine == QString("SWAP_FILE"))
+		qMsg = QString(tr2i18n("Allocating swap file... [2/13]"));
+
+	else if (qLine == QString("CREATE_SWAP"))
+		qMsg = QString(tr2i18n("Creating swap file system... [3/13]"));
+
+	else if (qLine == QString("SWAP_ON"))
+		qMsg = QString(tr2i18n("Activating swap file... [4/13]"));
+
+	else if (qLine == QString("HOME_FILE"))
+		qMsg = QString(tr2i18n("Allocating home file... [5/13]"));
+
+	else if (qLine == QString("DEVICE_SEARCH"))
+		qMsg = QString(tr2i18n("Searching for a free device... [6/13]"));
+
+	else if (qLine == QString("CREATE_HOME"))
+		qMsg = QString(tr2i18n("Creating home file system... [7/13]"));
+
+	else if (qLine == QString("TUNE_HOME"))
+		qMsg = QString(tr2i18n("Tunning home gile system... [8/13]"));
+
+	else if (qLine == QString("OWN_CHANGE"))
+		qMsg = QString(tr2i18n("Changing ownership... [9/13]"));
+
+	else if (qLine == QString("COPY_HOME"))
+		qMsg = QString(tr2i18n("Copying home... [10/13]"));
+
+	else if (qLine == QString("RELEASE_HOME"))
+		qMsg = QString(tr2i18n("Releasing home device... [11/13]"));
+
+	else if (qLine == QString("ACTIVATE_HOME"))
+		qMsg = QString(tr2i18n("Activating home... [12/13]"));
+
+	else if (qLine == QString("FINISH"))
+		qMsg = QString(tr2i18n("Finishing... [13/13]"));
+
+	else qMsg = QString(tr2i18n("Working... [N/A]"));
+
+	status->setText(qMsg);
 }
--- confwiz/scripts/config.sh
+++ confwiz/scripts/config.sh
@@ -9,19 +9,28 @@ MNT_PT=$3
 
 HOME_SIZE=100
 
+echo "DIR_CREATE" > /tmp/.progress
 cd $MNT_PT
 mkdir kinneret
 
 # create swap
 if [ $SWAP_SIZE -gt 0 ]
 then
+	echo "SWAP_FILE" > /tmp/.progress
 	dd if=/dev/zero of="$MNT_PT/kinneret/.swap" bs=1M count=$SWAP_SIZE;
+
+	echo "CREATE_SWAP" > /tmp/.progress
 	mkswap "$MNT_PT/kinneret/.swap"
+
+	echo "SWAP_ON" > /tmp/.progress
 	swapon "$MNT_PT/kinneret/.swap"
 fi
 
-# create the ext3 file (20megs)
+# create the ext3 file
+echo "HOME_FILE" > /tmp/.progress
 dd if=/dev/zero of="$MNT_PT/kinneret/.config" bs=1M count=$HOME_SIZE
+
+echo "DEVICE_SEARCH" > /tmp/.progress
 devnum=-1
 while [[ $? -ne 0 && $devnum -lt 8 || $devnum -eq -1 ]]
 do
@@ -34,13 +43,18 @@ then
 	exit 1
 fi
 
+echo "CREATE_HOME" > /tmp/.progress
 mkfs -t ext3 /dev/loop$devnum
+
+echo "TUNE_HOME" > /tmp/.progress
 # tune2fs -L KINNERET /dev/loop$devnum		# causes a hang... why?
 
+echo "OWN_CHANGE" > /tmp/.progress
 cd /mnt
 mkdir tmpcfg
 chown knoppix:knoppix /mnt/tmpcfg
 
+echo "COPY_HOME" > /tmp/.progress
 # mount ext3 to /mnt/tmpcfg
 mount /dev/loop$devnum /mnt/tmpcfg
 
@@ -48,14 +62,17 @@ mount /dev/loop$devnum /mnt/tmpcfg
 cd /home/knoppix
 cp -a -f --preserve=all . /mnt/tmpcfg 2>&1
 
+echo "RELEASE_HOME" > /tmp/.progress
 # umount tmpcfg
 umount /dev/loop$devnum
 rmdir /mnt/tmpcfg
 losetup -d /dev/loop$devnum
 
+echo "ACTIVATE_HOME" > /tmp/.progress
 # mount ext3 to /home/knoppix
 mount "$MNT_PT/kinneret/.config" /home/knoppix -t ext3 -o defaults,rw,loop 2>&1 > /dev/null
 
+echo "FINISH" > /tmp/.progress
 # change premissions
 chown -R knoppix:knoppix /home/knoppix
 
