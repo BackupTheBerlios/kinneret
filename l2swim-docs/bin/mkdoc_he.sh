b98ea2650d43300d8a341178baf7004ee7ec2876
dovix <dovix>
2003-07-22 21:23:25 +0000

    *** empty log message ***

--- l2swim-docs/bin/mkdoc_he.sh
+++ l2swim-docs/bin/mkdoc_he.sh
@@ -41,7 +41,7 @@ xsltproc -o ../doc/he/thoughts_3.html ../etc/l2swim.xsl ../src/he/thoughts_3.xml
 xsltproc -o ../doc/he/thoughts_4.html ../etc/l2swim.xsl ../src/he/thoughts_4.xml
 xsltproc -o ../doc/he/thoughts_5.html ../etc/l2swim.xsl ../src/he/thoughts_5.xml
 xsltproc -o ../doc/he/first_time.html ../etc/l2swim.xsl ../src/he/first_time.xml
-xsltproc -o ../doc/he/apps_list.html ../etc/l2swim.xsl ../src/he/apps_list.xml
+#xsltproc -o ../doc/he/apps_list.html ../etc/l2swim.xsl ../src/he/apps_list.xml
 xsltproc -o ../doc/he/social_contract.html ../etc/l2swim.xsl ../src/he/social_contract.xml
 xsltproc -o ../doc/he/about_kinneret.html ../etc/l2swim.xsl ../src/he/about_kinneret.xml
 xsltproc -o ../doc/he/man_enigma.html ../etc/l2swim.xsl ../src/he/man_enigma.xml
