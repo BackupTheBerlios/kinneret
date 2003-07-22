eace41d5478e15f2e1085fae6db073e35b6452e8
nirro <nirro>
2003-07-22 22:29:26 +0000

    *** empty log message ***

--- l2swim-docs/bin/mkdoc.sh
+++ l2swim-docs/bin/mkdoc.sh
@@ -1,6 +1,11 @@
 echo You must be root in order to copy files to /opt/kinneret
 . mkdoc_he.sh
 . mkdoc_en.sh
+mkdir /opt/kinneret/l2swim/doc/he -p
+mkdir /opt/kinneret/l2swim/doc/en -p
+mkdir /opt/kinneret/l2swim/doc/image -p
+mkdir /opt/kinneret/l2swim/etc -p
+mkdir /opt/kinneret/images -p
 cp ../doc/he/* /opt/kinneret/l2swim/doc/he -f
 cp ../doc/en/* /opt/kinneret/l2swim/doc/en -f
 cp ../doc/image/* /opt/kinneret/l2swim/doc/image -f
