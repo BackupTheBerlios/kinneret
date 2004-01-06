8270d9f1995915eefce01a799930eee6faa47450
nirro <nirro>
2004-01-06 20:16:52 +0000

    added debian support

---
+++ knrtheme/debian/knrtheme-default.ex
@@ -0,0 +1,10 @@
+# Defaults for knrtheme initscript
+# sourced by /etc/init.d/knrtheme
+# installed at /etc/default/knrtheme by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ knrtheme/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(knrtheme):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="knrtheme" command="/usr/bin/knrtheme"
