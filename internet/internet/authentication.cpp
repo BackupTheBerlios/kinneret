315301f9e9cb3ee185ca385280b0b77cf012188f
z9u2k <z9u2k>
2004-01-16 11:31:46 +0000

    Fixed [chap|pap]-secrets restore bug

--- internet/internet/authentication.cpp
+++ internet/internet/authentication.cpp
@@ -21,14 +21,21 @@ void Authentication::MakeInit(ostream &stream) const
 	stream << "# backup" << endl;
 	stream << "cp /etc/ppp/pap-secrets /etc/ppp/pap-secrets.bak" << endl;
 	stream << "cp /etc/ppp/chap-secrets /etc/ppp/chap-secrets.bak" << endl;
-	
+
 	stream << "echo '\"" << getUsername() << "\" * \"" << getPasswd() << "\"' > /etc/ppp/pap-secrets" << endl;
 	stream << "echo '\"" << getUsername() << "\" * \"" << getPasswd() << "\"' > /etc/ppp/chap-secrets" << endl;
 }
 
 void Authentication::MakeBoot(ostream &stream) const { stream << endl; }
 void Authentication::MakeConnect(ostream &stream) const { stream << endl; }
-void Authentication::MakeDisconnect(ostream &stream) const { stream << endl; }
+
+void Authentication::MakeDisconnect(ostream &stream) const
+{
+	stream << "# restore" << endl;
+	stream << "cp /etc/ppp/pap-secrets.bak /etc/ppp/pap-secrets" << endl;
+	stream << "cp /etc/ppp/chap-secrets.bak /etc/ppp/chap-secrets" << endl;
+}
+
 void Authentication::MakeDone(ostream &stream) const { stream << endl; }
 
 
