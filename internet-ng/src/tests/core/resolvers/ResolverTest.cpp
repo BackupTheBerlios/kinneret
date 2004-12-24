7a0ab311a2b062794c792c8840d61da561414173
z9u2k <z9u2k>
2004-12-24 15:48:30 +0000

    Tests resolving (of a driver)

--- internet-ng/src/tests/core/resolvers/ResolverTest.cpp
+++ internet-ng/src/tests/core/resolvers/ResolverTest.cpp
@@ -1,23 +1,58 @@
 #include "core/resolvers/Resolver.h"
 #include "core/utils/Log.h"
 #include "core/utils/Config.h"
+#include "core/utils/utils.h"
 #include "core/exception/exception.h"
+#include "core/xml/xml.h"
+
+#include <string>
 
 using namespace core::exception;
 using namespace core::resolvers;
 using namespace core::utils;
-/*
-void testConstructor() {
+using namespace core::xml;
+using namespace xercesc;
+using namespace std;
+
+void testResolver() {
     Resolver *resolver = 0;
+    // Load test resolver
+    resolver = new Resolver(
+        Config::getDatabasePath() +
+            Config::getResolversDirectoryName() + "/driver.xml",
+        Config::getDatabasePath() +
+            Config::getDriversDirectoryName() + "/generic-pptp-cable.xml");
 
-    resolver = new Resolver();
+    Log::info("Attempting to resolve valid param...");
+    if (resolver->canResolve("name")) {
+        Log::info("Resolveable");
+    } else {
+        Log::info("Unresolveable");
+        throw FatalException("Failure in loading resolver!");
+    }
+
+    Log::info("name = " + resolver->resolvParam("name"));
+
+    Log::info("Attempting to resolve invalid param...");
+    if (resolver->canResolve("__name")) {
+        Log::info("Resolveable");
+        throw FatalException("Failure in loading resolver!");
+    } else {
+        Log::info("Unresolveable");
+    }
+
+    delete resolver;
 }
-*/
-int main() {
-    Log::create(Log::VERBOSE);
 
+int main() {
+    // Initialize logger
+    Log::create(Log::VERBOSE);
+    initXML();
+    
+
+    // Load config
     try {
-        Config::create("/home/z9u2k/projects/internet-ng/database/test.conf");
+        Config::create("database/test.conf");
     } catch (const FileNotFoundException &ex) {
         Log::fatal(ex.what());
         return -1;
@@ -28,14 +63,29 @@ int main() {
         Log::fatal(ex.what());
         return -1;
     }
-    /*
+
+    int rc = 0;
+
+    // Do test
     try {
-        testConstructor();
+        testResolver();
+    } catch (const XMLException &ex) {
+        Log::fatal(getExceptionString(ex));
+        rc = -1;
+    } catch (const DOMException &ex) {
+        Log::fatal(getExceptionString(ex));
+        rc = -1;
+    } catch (const FatalException &ex) {
+        Log::fatal(string("FatalException: ") + ex.what());    
+        rc = -1;
     } catch (...) {
+        Log::fatal("Unknown exception");
+        rc = -1;
     }
-    */
+
     Config::release();
+    releaseXML();
     Log::release();
 
-    return 0;
+    return rc;
 }
